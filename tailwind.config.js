56af0a16bd454a5504e73507fc8953240895a2f1
yangharry <myid7771@naver.com>
2024-02-13 12:15:59 +0900

    feat: 내 정보

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/react-tailwindcss-datepicker/dist/index.esm.js'],
   theme: {
     extend: {},
+    colors: {
+      customblack: '#141617',
+      white: '#ffffff',
+      black: '#000000',
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 };
