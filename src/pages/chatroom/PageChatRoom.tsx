9857cbd964d5ca76ded44de45b82315ac33c4457
yangharry <myid7771@naver.com>
2024-02-17 22:45:40 +0900

    feat: file tree

--- src/pages/chatroom/PageChatRoom.tsx
+++ src/pages/chatroom/PageChatRoom.tsx
@@ -1,20 +1,112 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import * as StompJs from '@stomp/stompjs';
 
-function PageChatRoom() {
+interface PageChatRoomProps {
+  roomId: string;
+}
+
+function PageChatRoom({ roomId }: PageChatRoomProps) {
+  const chatRef = useRef<HTMLDivElement[]>([]);
+  const client = useRef<StompJs.Client | null>(null);
+  const [sender, setSender] = useState('');
   const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<{ type: string; sender: string; message: string }[]>([]);
+
+  useEffect(() => {
+    const storedSender = localStorage.getItem('wschat.sender');
+    if (storedSender) {
+      setSender(storedSender);
+    }
+
+    connect();
+
+    return () => {
+      if (client.current) {
+        disconnect();
+      }
+    };
+  }, [roomId]); // roomId를 의존성 배열에 추가
+
+  useEffect(() => {
+    handleScroll(chatRef.current[messages.length - 1]);
+  }, [messages]); // messages가 업데이트될 때마다 호출
+
+  const connect = () => {
+    client.current = new StompJs.Client({
+      brokerURL: 'ws://localhost:8080/ws-stomp/websocket', // 웹소켓 서버로 직접 접속
+      // webSocketFactory: () => new SockJS('/ws-stomp'), // proxy를 통한 접속
+      // connectHeaders: {
+      //   'auth-token': 'spring-chat-auth-token',
+      // },
+      debug: (str) => {
+        console.log(str);
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+      onConnect: () => {
+        subscribe();
+      },
+      onStompError: (frame) => {
+        console.error(frame);
+      },
+    });
+
+    client.current.activate();
+  };
+
+  const disconnect = () => {
+    if (client.current) {
+      client.current.deactivate();
+    }
+  };
+
+  const subscribe = () => {
+    if (client.current) {
+      client.current.subscribe(`/sub/chat/room/${roomId}`, (message) => {
+        console.log(message.body);
+        const newMessage = JSON.parse(message.body);
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+      });
+    }
+  };
+
+  const publish = (e: any) => {
+    e.preventDefault();
+    if (client.current && client.current.connected) {
+      client.current.publish({
+        destination: '/pub/chat/message',
+        body: JSON.stringify({ type: 'TALK', roomId: roomId, sender: sender, message: message }),
+      });
+    }
+    setMessage(''); // 메시지 전송 후 입력 필드 초기화
+  };
+
+  const handleScroll = (ref: HTMLDivElement | null) => {
+    if (ref) {
+      ref.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+  };
 
   return (
     <div className="h-full w-full p-5" style={{ backgroundColor: '#141617' }}>
-      <div className="w-full mb-5" style={{ height: 'calc(100% - 70px)' }}></div>
+      <div className="w-full mb-5 overflow-y-scroll scrollbar-hide" style={{ height: 'calc(100% - 70px)' }}>
+        {messages.map((message, index) => (
+          <div
+            ref={(el: HTMLDivElement) => {
+              chatRef.current[index] = el;
+            }}
+            key={index}
+            className={`${
+              message.sender === localStorage.getItem('wschat.sender') ? 'text-right' : ' text-left'
+            }  text-white text-sm`}
+          >
+            {message.sender} - {message.message}
+          </div>
+        ))}
+      </div>
       <div className="w-full h-[50px] rounded-[20px]" style={{ backgroundColor: '#2F3336' }}>
-        <form
-          className="w-full h-full flex justify-center items-center"
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log(message);
-            setMessage('');
-          }}
-        >
+        <form className="w-full h-full flex justify-center items-center" onSubmit={publish}>
           <input
             className=" flex-grow h-[44px] text-white caret-white px-2 rounded-l-[20px] "
             style={{ backgroundColor: '#2F3336' }}
