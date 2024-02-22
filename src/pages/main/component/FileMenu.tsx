9a9fc5cb682a64ea8f4f8d5eca1397dc088a2aa6
yangharry <myid7771@naver.com>
2024-02-22 12:42:54 +0900

    add: prerrtier

--- src/pages/main/component/FileMenu.tsx
+++ src/pages/main/component/FileMenu.tsx
@@ -1,76 +1,81 @@
 import { useState } from 'react';
-import { useAppDispatch } from '../../../store/hooks';
-import { fileAdd, folderAdd } from '../../../store/reducers/fileTreeSlice';
+
+import { useAppDispatch } from '@/store/hooks';
+import { fileAdd, folderAdd } from '@/store/reducers/fileTreeSlice';
+
 interface FileTree {
-  id: number | string;
-  parent: number | string;
-  droppable?: boolean;
-  text: string;
-  data?: FileTreeData;
+    id: number | string;
+    parent: number | string;
+    droppable?: boolean;
+    text: string;
+    data?: FileTreeData;
 }
 
 interface FileTreeData {
-  fileType: string;
-  fileSize: string;
+    fileType: string;
+    fileSize: string;
 }
 
 interface Props {
-  selectedFile: FileTree;
+    selectedFile: FileTree;
 }
 
 function FileMenu({ selectedFile }: Props) {
-  const dispatch = useAppDispatch();
+    const dispatch = useAppDispatch();
 
-  const [isHovering, setIsHovering] = useState(false);
+    const [isHovering, setIsHovering] = useState(false);
 
-  function mouseOverHandler() {
-    setIsHovering(true);
-  }
-  function mouseOutHandler() {
-    setIsHovering(false);
-  }
-  return (
-    <div
-      className="w-[260px] absolute text-sm border-black z-30 text-white top-[32px] left-0"
-      style={{ border: 'solid 1px #141617', backgroundColor: '#2F3336' }}
-    >
-      <div
-        className="h-[30px] flex items-center justify-between hover:bg-black px-5 relative"
-        onMouseOver={mouseOverHandler}
-        onMouseOut={mouseOutHandler}
-      >
-        <div className="text-white">새로 만들기</div>
-        <div className="material-symbols-outlined">arrow_right</div>
-        {isHovering && (
-          <div
-            className="w-[200px] absolute text-sm border-black z-30 text-white left-[235px] top-[-1px]"
+    function mouseOverHandler() {
+        setIsHovering(true);
+    }
+    function mouseOutHandler() {
+        setIsHovering(false);
+    }
+    return (
+        <div
+            className="w-[260px] absolute text-sm border-black z-30 text-white top-[32px] left-0"
             style={{ border: 'solid 1px #141617', backgroundColor: '#2F3336' }}
-          >
+        >
             <div
-              className="h-[30px] flex items-center justify-between hover:bg-black px-5"
-              onClick={() => {
-                dispatch(fileAdd(selectedFile));
-              }}
+                className="h-[30px] flex items-center justify-between hover:bg-black px-5 relative"
+                onMouseOver={mouseOverHandler}
+                onMouseOut={mouseOutHandler}
             >
-              새 파일
+                <div className="text-white">새로 만들기</div>
+                <div className="material-symbols-outlined">arrow_right</div>
+                {isHovering && (
+                    <div
+                        className="w-[200px] absolute text-sm border-black z-30 text-white left-[235px] top-[-1px]"
+                        style={{
+                            border: 'solid 1px #141617',
+                            backgroundColor: '#2F3336',
+                        }}
+                    >
+                        <div
+                            className="h-[30px] flex items-center justify-between hover:bg-black px-5"
+                            onClick={() => {
+                                dispatch(fileAdd(selectedFile));
+                            }}
+                        >
+                            새 파일
+                        </div>
+                        <div
+                            className="h-[30px] flex items-center justify-between hover:bg-black px-5"
+                            onClick={() => {
+                                dispatch(folderAdd(selectedFile));
+                            }}
+                        >
+                            새 폴더
+                        </div>
+                    </div>
+                )}
             </div>
-            <div
-              className="h-[30px] flex items-center justify-between hover:bg-black px-5"
-              onClick={() => {
-                dispatch(folderAdd(selectedFile));
-              }}
-            >
-              새 폴더
+            <div className="h-[30px] flex items-center justify-between hover:bg-black px-5">
+                <div className="text-white">파일 열기</div>
+                <div className="text-gray-400 pr-2">Ctrl + O</div>
             </div>
-          </div>
-        )}
-      </div>
-      <div className="h-[30px] flex items-center justify-between hover:bg-black px-5">
-        <div className="text-white">파일 열기</div>
-        <div className="text-gray-400 pr-2">Ctrl + O</div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default FileMenu;
--- src/pages/main/component/MyTreeView.tsx
+++ src/pages/main/component/MyTreeView.tsx
@@ -1,125 +1,152 @@
-import { Tree, getBackendOptions, MultiBackend } from '@minoru/react-dnd-treeview';
-import { DndProvider } from 'react-dnd';
-import TypeIcon from './TypeIcon';
-import { useAppDispatch, useAppSelector } from '../../../store/hooks';
-import { fileTreehandleDrop, setFileText } from '../../../store/reducers/fileTreeSlice';
 import { Dispatch, SetStateAction } from 'react';
 
+import {
+    MultiBackend,
+    Tree,
+    getBackendOptions,
+} from '@minoru/react-dnd-treeview';
+import { DndProvider } from 'react-dnd';
+
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import {
+    fileTreehandleDrop,
+    setFileText,
+} from '@/store/reducers/fileTreeSlice';
+
+import TypeIcon from './TypeIcon';
+
 interface FileTree {
-  id: number | string;
-  parent: number | string;
-  droppable?: boolean;
-  text: string;
-  data?: FileTreeData;
+    id: number | string;
+    parent: number | string;
+    droppable?: boolean;
+    text: string;
+    data?: FileTreeData;
 }
 
 interface FileTreeData {
-  fileType: string;
-  fileSize: string;
+    fileType: string;
+    fileSize: string;
 }
 
 interface Props {
-  selectedFile: FileTree;
-  setSelectedFile: Dispatch<SetStateAction<FileTree>>;
-  setIsEditFileName: Dispatch<SetStateAction<boolean>>;
-  isEditFileName: boolean;
-  setIsTreeRightClick: Dispatch<SetStateAction<boolean>>;
-  setTreeRightClickXY: Dispatch<
-    React.SetStateAction<{
-      x: number;
-      y: number;
-    }>
-  >;
+    selectedFile: FileTree;
+    setSelectedFile: Dispatch<SetStateAction<FileTree>>;
+    setIsEditFileName: Dispatch<SetStateAction<boolean>>;
+    isEditFileName: boolean;
+    setIsTreeRightClick: Dispatch<SetStateAction<boolean>>;
+    setTreeRightClickXY: Dispatch<
+        React.SetStateAction<{
+            x: number;
+            y: number;
+        }>
+    >;
 }
 
 function MyTreeView({
-  selectedFile,
-  isEditFileName,
-  setSelectedFile,
-  setIsEditFileName,
-  setIsTreeRightClick,
-  setTreeRightClickXY,
+    selectedFile,
+    isEditFileName,
+    setSelectedFile,
+    setIsEditFileName,
+    setIsTreeRightClick,
+    setTreeRightClickXY,
 }: Props) {
-  const dispatch = useAppDispatch();
+    const dispatch = useAppDispatch();
 
-  const treeData: FileTree[] = useAppSelector((state) => state.fileTree);
-  const handleDrop = (newTreeData: any) => dispatch(fileTreehandleDrop(newTreeData));
-  const handleDoubleClick = () => {
-    setIsEditFileName(true);
-  };
+    const treeData: FileTree[] = useAppSelector((state) => state.fileTree);
+    const handleDrop = (newTreeData: any) =>
+        dispatch(fileTreehandleDrop(newTreeData));
+    const handleDoubleClick = () => {
+        setIsEditFileName(true);
+    };
 
-  const handleRightClick = (e: any, node: any) => {
-    e.preventDefault();
-    setSelectedFile(node);
-    setTreeRightClickXY({ x: e.clientX, y: e.clientY });
-    setIsTreeRightClick(true);
-  };
+    const handleRightClick = (e: any, node: any) => {
+        e.preventDefault();
+        setSelectedFile(node);
+        setTreeRightClickXY({ x: e.clientX, y: e.clientY });
+        setIsTreeRightClick(true);
+    };
 
-  return (
-    <DndProvider backend={MultiBackend} options={getBackendOptions()}>
-      <Tree
-        tree={treeData}
-        rootId={0}
-        onDrop={handleDrop}
-        render={(node, { depth, isOpen, onToggle }) => (
-          <div
-            className={`${
-              selectedFile.id === node.id ? 'bg-[#2c3e59] border border-[#5094fa]' : ''
-            } flex items-center h-6 text-[12px] cursor-pointer pl-2 hover:bg-[#2F3336] z-20`}
-            style={{ marginLeft: depth * 10 }}
-            onClick={() => {
-              onToggle();
-              if (selectedFile.id !== node.id) setIsEditFileName(false);
-              setSelectedFile(node);
-            }}
-          >
-            {node.droppable && (
-              <div className="flex items-center">
-                {isOpen ? (
-                  <div className="material-symbols-outlined text-[20px]">keyboard_arrow_down</div>
-                ) : (
-                  <div className="material-symbols-outlined text-[20px]">keyboard_arrow_right</div>
+    return (
+        <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+            <Tree
+                tree={treeData}
+                rootId={0}
+                onDrop={handleDrop}
+                render={(node, { depth, isOpen, onToggle }) => (
+                    <div
+                        className={`${
+                            selectedFile.id === node.id
+                                ? 'bg-[#2c3e59] border border-[#5094fa]'
+                                : ''
+                        } flex items-center h-6 text-[12px] cursor-pointer pl-2 hover:bg-[#2F3336] z-20`}
+                        style={{ marginLeft: depth * 10 }}
+                        onClick={() => {
+                            onToggle();
+                            if (selectedFile.id !== node.id)
+                                setIsEditFileName(false);
+                            setSelectedFile(node);
+                        }}
+                    >
+                        {node.droppable && (
+                            <div className="flex items-center">
+                                {isOpen ? (
+                                    <div className="material-symbols-outlined text-[20px]">
+                                        keyboard_arrow_down
+                                    </div>
+                                ) : (
+                                    <div className="material-symbols-outlined text-[20px]">
+                                        keyboard_arrow_right
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                        <TypeIcon
+                            droppable={node.droppable}
+                            isOpen={isOpen}
+                            fileType={node.data?.fileType}
+                        />
+                        <div className="w-full h-full pl-1 flex items-center">
+                            {isEditFileName && selectedFile.id === node.id ? (
+                                <input
+                                    type="text"
+                                    value={node.text}
+                                    className="w-full h-full bg-inherit outline-none flex items-center"
+                                    onFocus={(e) => e.target.select()}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Enter') {
+                                            setIsEditFileName(false);
+                                        }
+                                    }}
+                                    onChange={(e) => {
+                                        e.preventDefault();
+                                        dispatch(
+                                            setFileText({
+                                                id: node.id,
+                                                text: e.target.value,
+                                            }),
+                                        );
+                                    }}
+                                    onContextMenu={(e) => {
+                                        handleRightClick(e, node);
+                                    }}
+                                />
+                            ) : (
+                                <div
+                                    className="w-full h-full bg-inherit flex items-center "
+                                    onDoubleClick={handleDoubleClick}
+                                    onContextMenu={(e) => {
+                                        handleRightClick(e, node);
+                                    }}
+                                >
+                                    {node.text}
+                                </div>
+                            )}
+                        </div>
+                    </div>
                 )}
-              </div>
-            )}
-            <TypeIcon droppable={node.droppable} isOpen={isOpen} fileType={node.data?.fileType} />
-            <div className="w-full h-full pl-1 flex items-center">
-              {isEditFileName && selectedFile.id === node.id ? (
-                <input
-                  type="text"
-                  value={node.text}
-                  className="w-full h-full bg-inherit outline-none flex items-center"
-                  onFocus={(e) => e.target.select()}
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                      setIsEditFileName(false);
-                    }
-                  }}
-                  onChange={(e) => {
-                    e.preventDefault();
-                    dispatch(setFileText({ id: node.id, text: e.target.value }));
-                  }}
-                  onContextMenu={(e) => {
-                    handleRightClick(e, node);
-                  }}
-                />
-              ) : (
-                <div
-                  className="w-full h-full bg-inherit flex items-center "
-                  onDoubleClick={handleDoubleClick}
-                  onContextMenu={(e) => {
-                    handleRightClick(e, node);
-                  }}
-                >
-                  {node.text}
-                </div>
-              )}
-            </div>
-          </div>
-        )}
-      />
-    </DndProvider>
-  );
+            />
+        </DndProvider>
+    );
 }
 
 export default MyTreeView;
